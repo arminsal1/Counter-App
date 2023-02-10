dcdb9b0633215af14fd2cb563392bb4370663678
calm-rad <salehipour1@gmail.com>
2023-02-09 19:40:03 -0500

    Change functions from variables

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var maxCount = 100;
 var minCount = 20;
 var currentCount = 0;
 
-window.onload = function() {
-  applyTapCountHTML();
-};
-
-const applyTapCountHTML = () => {
+function applyTapCountHTML() {
   var button = document.getElementById('countButton');
   button.innerHTML = currentCount;
 };
 
-const addCount = (event) => {
+function addCount(event) {
   currentCount += 1;
+  applyTapCountHTML();
+};
+
+window.onload = function() {
   applyTapCountHTML();
 };

18ac4238c8111cee934fe234f8d276d7e8a101cf
calm-rad <salehipour1@gmail.com>
2023-02-13 23:14:59 -0500

    Add more HTML elements

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+var currentCount = 0;
+var startCount = minCount;
+
 var maxCount = 100;
 var minCount = 20;
-var currentCount = 0;
+
+var playing = false;
 
 function applyTapCountHTML() {
-  console.log('applyTapCountHTML...', currentCount);
   var button = document.getElementById('countButton');
-  console.log('button...', button.innerHTML);
   button.innerHTML = currentCount;
 };
 
@@ -14,7 +16,106 @@ function addCount() {
   applyTapCountHTML();
 };
 
+function onlyNumberKey(evt) {
+  return (/^-?\d+$/.test(evt.key) || evt.which == 8) ? true : false;
+}
+
+const audioCtx = new AudioContext();
+
+let tempo = 60.0;
+const bpmControl = document.querySelector("#bpm");
+
+bpmControl.addEventListener(
+  "input",
+  (ev) => {
+    tempo = parseInt(ev.target.value, 10);
+  },
+  false
+);
+
+const lookahead = 25.0; // How frequently to call scheduling function (in milliseconds)
+const scheduleAheadTime = 0.1; // How far ahead to schedule audio (sec)
+
+let currentNote = 0;
+let nextNoteTime = 0.0; // when the next note is due.
+
+function nextNote() {
+  const secondsPerBeat = 60.0 / tempo;
+
+  nextNoteTime += secondsPerBeat; // Add beat length to last beat time
+
+  // Advance the beat number, wrap to zero when reaching 4
+  currentNote = (currentNote + 1) % 4;
+}
+
+const notesInQueue = [];
+
+let attackTime = 0.2;
+const attackControl = document.querySelector("#attack");
+attackControl.addEventListener(
+  "input",
+  (ev) => {
+    attackTime = parseInt(ev.target.value, 10);
+  },
+  false
+);
+
+let releaseTime = 0.5;
+const releaseControl = document.querySelector("#release");
+releaseControl.addEventListener(
+  "input",
+  (ev) => {
+    releaseTime = parseInt(ev.target.value, 10);
+  },
+  false
+)
+
+const sweepLength = 2;
+function playSweep(time) {
+  const osc = new OscillatorNode(audioCtx, {
+    frequency: 380,
+    type: "custom",
+    periodicWave: wave,
+  });
+
+  const sweepEnv = new GainNode(audioCtx);
+  sweepEnv.gain.cancelScheduledValues(time);
+  sweepEnv.gain.setValueAtTime(0, time);
+  sweepEnv.gain.linearRampToValueAtTime(1, time + attackTime);
+  sweepEnv.gain.linearRampToValueAtTime(0, time + sweepLength - releaseTime);
+
+  osc.connect(sweepEnv).connect(audioCtx.destination);
+  osc.start(time);
+  osc.stop(time + sweepLength);
+}
+
+
+function scheduleNote(beatNumber, time) {
+  // Push the note on the queue, even if we're not playing.
+  notesInQueue.push({ note: beatNumber, time });
+
+  if (pads[0].querySelectorAll("input")[beatNumber].checked) {
+    playSweep(time);
+  }
+  if (pads[1].querySelectorAll("input")[beatNumber].checked) {
+    playPulse(time);
+  }
+  if (pads[2].querySelectorAll("input")[beatNumber].checked) {
+    playNoise(time);
+  }
+}
+
+let timerID;
+function scheduler() {
+  // While there are notes that will need to play before the next interval,
+  // schedule them and advance the pointer.
+  while (nextNoteTime < audioCtx.currentTime + scheduleAheadTime) {
+    scheduleNote(currentNote, nextNoteTime);
+    nextNote();
+  }
+  timerID = setTimeout(scheduler, lookahead);
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOM fully loaded and parsed');
   applyTapCountHTML();
 });
